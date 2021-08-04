4009cbd07d0a52791f6624935c74452c841ef37e
Aarav-Malde <64626938+Aarav-Malde@users.noreply.github.com>
2021-08-04 07:42:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import BarcodeScreen from './screens/BarcodeScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <BarcodeScreen></BarcodeScreen>
+      
+    );
+  }
+}
+
+
